778812d03a45bd056a7f7ce20055019e7c40ff0c
caleb <shawcaleb1688@gmail.com>
2023-07-27 00:25:46 -0400

    added state to the form

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactDOM } from "react-dom";
 
 
 const Order = () => {
 
+  const [formData, setFormData] = useState({
+
+    name: '',
+    pizzaSize: 'Option',
+    pepperoni: false,
+    sausage: false,
+    pineapple: false,
+    mushrooms: false,
+    bacon: false,
+    extraCheese: false,
+    greenPeppers: false,
+    bananaPeppers: false,
+    specialInstructions: '',
+
+  })
+
+  const handleChange = (event) => {
+    const {name, value, type, checked} = event.target
+    setFormData(prevFormData => {
+        return {
+            ...prevFormData,
+            [name]: type === "checkbox" ? checked : value
+        }
+    })
+}
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData)
+  }
+
   return (
     <div className="formContainer">
-    <form id="pizza-form">
+    <form id="pizza-form" onSubmit={handleSubmit}>
       <label>Enter Name:
-        <input type="text" id="name-input"/>
+        <input type="text" id="name-input" name="name" value={formData.name}  onChange={handleChange}/>
       </label>
-      <label>
+      <br/>
+      <label htmlFor="pizzaDropdown">
         Pizza Size:
-          <select id="size-dropdown">
+          <select id="size-dropdown" name="pizzaDropdownValue" value={formData.pizzaDropdownValue}  onChange={handleChange}>
 
           <option value="option">--Select Option--</option>
 
@@ -27,44 +60,44 @@ const Order = () => {
     <div>
       <label>
         Pepperoni
-      <input type="checkbox" id="pepperoni" />
+      <input type="checkbox" id="pepperoni" name="pepperoni" checked={formData.pepperoni} onChange={handleChange}/>
       </label>
       <label>
         Sausage
-      <input type="checkbox" id="sausage" />
+      <input type="checkbox" id="sausage" name="sausage" checked={formData.sausage}  onChange={handleChange}/>
       </label>
       <label>
         Pineapple
-      <input type="checkbox" id="pineapple"/>
+      <input type="checkbox" id="pineapple" name="pineapple" checked={formData.pineapple} onChange={handleChange}/>
       </label>
       <label>
         Mushrooms
-      <input type="checkbox" id="mushrooms" />
+      <input type="checkbox" id="mushrooms" name="mushrooms" checked={formData.mushrooms} onChange={handleChange}/>
       </label>
       <label>
         Bacon
-      <input type="checkbox" id="bacon" />
+      <input type="checkbox" id="bacon" name="bacon" checked={formData.bacon}  onChange={handleChange}/>
       </label>
       <label>
         Extra Cheese
-      <input type="checkbox" id="extra-cheese"/>
+      <input type="checkbox" id="extra-cheese" name="extraCheese" checked={formData.extraCheese} onChange={handleChange}/>
       </label>
       <label>
         Green Peppers
-      <input type="checkbox" id="green-peppers" />
+      <input type="checkbox" id="green-peppers" name="greenPeppers" checked={formData.greenPeppers} onChange={handleChange} />
       </label>
       <label>
         Banana Peppers
-      <input type="checkbox" id="banana-peppers" />
+      <input type="checkbox" id="banana-peppers" name="bananaPeppers" checked={formData.bananaPeppers} onChange={handleChange} />
       </label>
 
     </div>
 
     <label>
       Special Instructions:
-      <input type="text" id="special-text" />
+      <input type="text" id="special-text" name="specialInstructions" value={formData.specialInstructions}  onChange={handleChange}/>
     </label>
-      <button>Add to Order</button>
+      <button name="addToOrder" id="order-button">Add to Order</button>
     </form>
     </div>
   )
